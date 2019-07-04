9f014e9bd7990763f509038d8797f42eccedb0c5
p-mh <pauline.miele@gmail.com>
2019-07-04 16:26:21 +0200

    client router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Route exact path="/" />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
